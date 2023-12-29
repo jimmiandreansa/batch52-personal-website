cfe0d532793e54819accb7e43a9b4c9e2ca58313
jimmiandreansa <jimmiandreansa11@gmail.com>
2023-12-29 21:19:29 +0700

    Modify JavaScript on the contact page

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,32 +1,43 @@
 function formSubmit(e) {
-  e.preventDefault()
+  // Mencegah reload browser
+  e.preventDefault();
 
+  // Mendapatkan nilai dari inputan user
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const phone = document.getElementById("phone").value;
   const subject = document.getElementById("subject").value;
   const message = document.getElementById("message").value;
 
-  if(name == "") {
-    return alert("Nama kolom nama harus diisi")
-  } else if(email == "") {
-    return alert("Kolom email tidak boleh kosong")
-  } else if(phone == "") {
-    return alert("Kolom No. Hp tidak boleh kosong")
-  } else if(message == "") {
-    return alert("Kolom message tidak boleh kosong")
+  // Membuat requirement
+  if (name == "") {
+    return alert("Nama kolom nama harus diisi");
+  } else if (email == "") {
+    return alert("Kolom email tidak boleh kosong");
+  } else if (phone == "") {
+    return alert("Kolom No. Hp tidak boleh kosong");
+  } else if (message == "") {
+    return alert("Kolom message tidak boleh kosong");
   }
 
-  console.log(name)
-  console.log(email)
-  console.log(phone)
-  console.log(subject)
-  console.log(message)
-
   // Programatically Link
-  let a = document.createElement("a")
+  let a = document.createElement("a");
 
-  a.href = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(message)}`
+  a.href = `
+  mailto:${email}?subject=${subject}&body=
+  ${message}
+  <br>
+  <br>
+  Nama: <b>${name}</b> <br>
+  No. HP: <b>${phone}</b> <br>
+  `;
 
-  a.click()
+  a.click();
+
+  // Mengosongkan form setelah mengirim form submit
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
+  document.getElementById("subject").value = "";
+  document.getElementById("message").value = "";
 }
