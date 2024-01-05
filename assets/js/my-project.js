000b1d46a30ebd3bd005f1908a7f480b6173a41a
jimmiandreansa <jimmiandreansa11@gmail.com>
2024-01-05 07:39:50 +0700

    Task Day-4: My Project Pages

--- assets/js/my-project.js
+++ assets/js/my-project.js
@@ -1,96 +1,104 @@
-const projects = [];
-
 function createCard(e) {
+  // Mencegah reload
   e.preventDefault();
 
-  const firstDate = document.getElementById("start-date").value;
-  const lastDate = document.getElementById("end-date").value;
+  let isiProject = JSON.parse(localStorage.getItem("projects")) || [];
 
+  if (localStorage.getItem("projects")) {
+    isiProject = JSON.parse(localStorage.getItem("projects"));
+  }
+
+  // Mengambil nilai inputan name & description
   const name = document.getElementById("name").value;
   const description = document.getElementById("description").value;
-  const technologies = [
-    
-  ];
+
+  // Mengambil nilai inputan image
   let image = document.getElementById("image").files;
   image = URL.createObjectURL(image[0]);
 
-  let startDate = new Date(firstDate);
-  let endDate = new Date(lastDate);
-  const time = Math.abs(endDate - startDate);
-
-  console.log(time);
-
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-  const months = Math.floor(time / (1000 * 60 * 60 * 24 * 30));
-
-  // const years = Math.floor(time / (1000 * 60 * 60 * 24 * 30 * 365));
-
-  // if(days < 30) {
-  //   return days + " Hari"
-  // } else if (days < 365) {
-  //   return months + " Bulan"
-  // } else if (days >= 365) {
-  //   return years + " Tahun"
-  // }
+  const nodeJs = document.getElementById("node-js").checked;
+  const nextJs = document.getElementById("next-js").checked;
+  const reactJs = document.getElementById("react-js").checked;
+  const vueJs = document.getElementById("vue-js").checked;
 
   const project = {
     name,
-    days,
-    months,
     description,
-    technologies,
+    nodeJs,
+    nextJs,
+    reactJs,
+    vueJs,
     image,
   };
 
-  projects.unshift(project);
+  isiProject.unshift(project);
+
+  console.log(isiProject);
+  saveProject();
+  renderProject();
+
+  // Fungsi untuk menyimpan project ke LocalStorage
+  function saveProject() {
+    localStorage.setItem("projects", JSON.stringify(isiProject));
+  }
+
+  // Fungsi untuk merender card project
+  function renderProject() {
+    let html = "";
+
+    for (let index = 0; index < isiProject.length; index++) {
+      let techIcons = "";
+
+      if (isiProject[index].nodeJs === true) {
+        techIcons += `<i class="fa-brands fa-node-js"></i>`;
+      }
+
+      if (isiProject[index].nextJs === true) {
+        techIcons += `<i class="fa-brands fa-js"></i>`;
+      }
+
+      if (isiProject[index].reactJs === true) {
+        techIcons += `<i class="fa-brands fa-react"></i>`;
+      }
+
+      if (isiProject[index].vueJs === true) {
+        techIcons += `<i class="fa-brands fa-vuejs"></i>`;
+      }
+
+      html += `
+        <div class="card">
+              <div class="card-img">
+                <a class="img-link" href="/project-x.html">
+                  <img
+                  src="${isiProject[index].image}"
+                  alt="My-Project"
+                />
+                </a>
+              </div>
+              <div class="card-information">
+                <div class="card-info-top">
+                  <h3 class="card-info-title">
+                    <a href="/project-x.html">
+                      ${isiProject[index].name} - 2024
+                    </a>
+                  </h3>
+                  <p class="card-info-time">Durasi: 2 Bulan</p>
+                </div>
+                <p class="card-info-middle">
+                  ${isiProject[index].description}
+                </p>
+                <div class="card-info-bottom">
+                  ${techIcons}
+                </div>
+                <div class="card-info-btn">
+                  <button class="card-btn">Edit</button>
+                  <button class="card-btn">Delete</button>
+                </div>
+              </div>
+            </div>`;
+    }
+    document.getElementById("input-project").innerHTML = html;
+  }
+
   renderProject();
 }
-
-function renderProject() {
-  let html = "";
-
-  for (let index = 0; index < projects.length; index++) {
-    html += `
-  <div class="card">
-            <div class="card-img">
-              <a class="img-link" href="/project-x.html">
-                <img
-                src="${projects[index].image}"
-                alt="My-Project"
-              />
-              </a>
-            </div>
-            <div class="card-information">
-              <div class="card-info-top">
-                <h3 class="card-info-title">
-                  <a href="/project-x.html">
-                    ${projects[index].name} - 2024
-                  </a>
-                </h3>
-                <p class="card-info-time">Durasi: ${projects[index].days < 30 ? projects[index].days : projects[index].months} ${
-      projects[index].days < 30 ? "Hari" : "Bulan"
-    }</p>
-              </div>
-              <p class="card-info-middle">
-                ${projects[index].description}
-              </p>
-              <div class="card-info-bottom">
-                <ion-icon
-                  class="card-icon"
-                  name="logo-google-playstore"
-                ></ion-icon>
-                <i class="fa-brands fa-js"></i>
-                <ion-icon class="card-icon" name="logo-android"></ion-icon>
-                <ion-icon class="card-icon" name="logo-javascript"></ion-icon>
-              </div>
-              <div class="card-info-btn">
-                <button class="card-btn">Edit</button>
-                <button class="card-btn">Delete</button>
-              </div>
-            </div>
-          </div>`;
-  }
-  document.getElementById("input-project").innerHTML = html;
-}
-
-renderProject();
