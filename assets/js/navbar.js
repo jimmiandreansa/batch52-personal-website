99c91366cebbcb5610c74ac7b7f2d182f24849d0
jimmiandreansa <jimmiandreansa11@gmail.com>
2024-01-07 21:45:32 +0700

    little maintenance

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,19 @@
 const hamburgerMenu = document.getElementById("hamburger-menu")
 const navbarLinks = document.getElementById("navbar-links")
 
+// Klik hamburger menu untuk menampilkan/menghilangkan navigation
 hamburgerMenu.addEventListener("click", () => {
   navbarLinks.classList.toggle("hidden")
+})
+
+// Klik diluar navigation untuk menghilangkan navigation hamburger
+document.addEventListener("click", function(e) {
+  if(!hamburgerMenu.contains(e.target) && !navbarLinks.contains(e.target)) {
+    navbarLinks.classList.add("hidden")
+  }
+})
+
+// Scroll untuk menghilangkan navigation hamburger
+document.addEventListener("scroll", function() {
+  navbarLinks.classList.add("hidden")
 })
